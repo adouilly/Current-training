126ec85893a426096a58ebdb4b7d2d1aa80c90e7
Antoine Douilly <antoine.douilly@gmail.com>
2025-05-30 09:38:05 +0200

    maj fichiers

---
+++ exercices/js/Exo 8/script.js
@@ -0,0 +1,21 @@
+// Exemple d'utilisation
+
+verifAge(12);
+
+function verifAge(age) {
+  if (age >= 18) {
+    console.log("Vous êtes majeur");
+  } else {
+    console.log("Vous êtes mineur");
+  }
+}
+
+levelVerif(15);
+
+function levelVerif(level) {
+  if (level >= 10) {
+    console.log("Vous avez un bon niveau");
+  } else {
+    console.log("Vous devez vous entraîner");
+  }
+}
