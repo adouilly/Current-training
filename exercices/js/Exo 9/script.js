3e8bd01e1f85dfaf14806dd16633ed47df8449d7
Antoine Douilly <antoine.douilly@gmail.com>
2025-05-30 11:17:04 +0200

    maj exo

---
+++ exercices/js/Exo 9/script.js
@@ -0,0 +1,10 @@
+// Exercice 9 - Afficher les nombres pairs de 1 à 20
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+};
+
+for (let i = 2; i <= 20; i += 2) {
+  console.log(i);
+};
